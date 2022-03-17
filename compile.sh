e3095dd4cea1662df14b20e71161baad4be74fa5
Ben Jolly <jollyb@landcareresearch.co.nz>
2022-03-18 11:34:56 +1300

    typo fixed by John

--- compile.sh
+++ compile.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+pdflatex arsa-beech-masting
+pdflatex arsa-beech-masting
 bibtex arsa-beech-masting
-latex arsa-beech-masting
+pdflatex arsa-beech-masting
 pdflatex arsa-beech-masting
